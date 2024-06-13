ba646f7d7d340e39f3425cb13972031f83961d35
Akash Kumar <163286401+codebyakash@users.noreply.github.com>
2024-06-13 23:40:35 +0530

    app by using express module

--- Practice/app.js
+++ Practice/app.js
@@ -1,11 +1,18 @@
-const express= require("express")
-
-const app= express()
-app.get("/", (req,res) => {
-    // console.log("hello from root side")
-    console.log(__dirname);
-    res.sendFile(__dirname+"/index.html")
-})
-app.listen(3000, () => {
-    console.log("server start at port no : 3000")
-})
+const express = require("express");
+const app = express();
+const port = 80;
+app.get("/", (req, res) => {
+    res.send("This my first express app");
+});
+app.get("/about", (req, res) => {
+    res.send("This my first express app about");
+});
+app.post("/about", (req, res) => {
+    res.send("This my first post express app about");
+});
+app.get("/this", (req, res) => {
+    res.status(404).send("This is not found");
+});
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+});
