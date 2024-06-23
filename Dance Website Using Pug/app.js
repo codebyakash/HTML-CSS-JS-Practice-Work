e6601860e2e7c5b18320dc4fe63aecdc5b27d9fd
Akash Kumar <163286401+codebyakash@users.noreply.github.com>
2024-06-23 22:34:37 +0530

    dance website using pug

---
+++ Dance Website Using Pug/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug');  //Set the template enginea as PushManager
+app.set('views', path.join(__dirname, 'views'));
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = { }
+    res.status(200).render('index.pug', params);
+})
+
+//Start the server
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
