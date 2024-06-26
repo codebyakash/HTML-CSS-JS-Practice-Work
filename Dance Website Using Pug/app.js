28ab7743e10c4d69fad4bcb2696eb36b4a771b4f
Akash Kumar <163286401+codebyakash@users.noreply.github.com>
2024-06-26 12:40:43 +0530

    Update app.js

--- Dance Website Using Pug/app.js
+++ Dance Website Using Pug/app.js
@@ -13,7 +13,12 @@ app.set('views', path.join(__dirname, 'views'));
 // ENDPOINTS
 app.get('/', (req, res)=>{
     const params = { }
-    res.status(200).render('index.pug', params);
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{
+    const params = { }
+    res.status(200).render('contact.pug', params);
 })
 
 //Start the server
