6245635fdcb7c8bf24d9621c5163d60656ec6b48
Akash Kumar <163286401+codebyakash@users.noreply.github.com>
2024-06-28 17:09:47 +0530

    mongoose using

--- Dance Website Using Pug/app.js
+++ Dance Website Using Pug/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser")
+mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
 const port = 8000;
+
+//define moongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+});
+
+const contact = mongoose.model('Kitten', contactSchema);
 
 app.use('/static', express.static('static'));
 app.use(express.urlencoded());
@@ -19,6 +33,17 @@ app.get('/', (req, res)=>{
 app.get('/contact', (req, res)=>{
     const params = { }
     res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, res)=>{
+    var myData = new contact(req.body);
+    myData.save().then(()=>{
+        res.send("this item has been saved to the database")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    })
+
+//     res.status(200).render('contact.pug');
 })
 
 //Start the server
